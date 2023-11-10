5bc7078bb7940a3aa514d88c54d97c72d8eae84e
Samara Almeida <samaraalmeida379@gmail.com>
2023-11-10 12:32:40 -0300

    exercício 1 - for com do/while

--- aula_04/src/aula_04/Exe01.java
+++ aula_04/src/aula_04/Exe01.java
@@ -1,6 +1,8 @@
 package aula_04;
+
 import java.util.Scanner;
 
+/*
 public class Exe01 {
 	static Scanner leia = new Scanner(System.in);
 	
@@ -14,7 +16,7 @@ public class Exe01 {
 			System.out.println("Digite o número final: ");
 			numero2 = leia.nextInt();
 			if(numero2 < numero1) {
-				System.out.println("O número final não pode ser menor que o número inicial: ");
+				System.out.println("Intervalo inválido!");
 			}
 		}while(numero2 < numero1);
 		
@@ -27,3 +29,30 @@ public class Exe01 {
 	}
 
 }
+*/
+
+public class Exe01 {
+	static Scanner leia = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int numero1, numero2;
+
+		System.out.println("Digite o número inicial: ");
+		numero1 = leia.nextInt();
+
+		System.out.println("Digite o número final: ");
+		numero2 = leia.nextInt();
+		if (numero2 < numero1) {
+			System.out.println("Intervalo inválido!");}
+
+			if (numero2 > numero1) {
+				for (int i = numero1; i <= numero2; i++) {
+					if (i % 3 == 0 && i % 5 == 0) {
+						System.out.println(i + " é múltiplo de 3 e 5");
+					}
+				}
+			}
+
+		}
+
+	}
