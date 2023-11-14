06073e8253ed99403c534f2a0d0a8b8fdaa4bdeb
Samara Almeida <samaraalmeida379@gmail.com>
2023-11-14 12:50:29 -0300

    Collections + exercícios

--- aula_06/src/aula_06/Exe03.java
+++ aula_06/src/aula_06/Exe03.java
@@ -3,12 +3,13 @@ package aula_06;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Exe03 {
 	static Scanner leia = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		HashSet<Integer> numeros = new HashSet<Integer>();
+		Set<Integer> numeros = new HashSet<Integer>();
 		int numeroDigitado;
 		
 		for(int i=0; i<10;i++) {
